aef45738ca9c48c39473f2e2ef7f3d67156c8b52
vis976 <vishalchaubey9628@gmail.com>
2022-06-11 16:42:40 +0530

    unit3_c2

--- scripts/voucher.js
+++ scripts/voucher.js
@@ -5,48 +5,64 @@ let user = JSON.parse(localStorage.getItem("user"))
 let wallet = document.getElementById("wallet_balance")
 wallet.innerText = user.amount;
 
+let data = JSON.parse(localStorage.getItem("purchase")) || []
+
+
 const url = `https://masai-vouchers-api.herokuapp.com/api/vouchers`
+async function getData() {
+    try{
+        let res = await fetch(url)
+        let data = await res.json()
+        append(data[0].vouchers)
 
-
-fetch(url).then (function (res){
-    return res.json()
-}).then(function(res){
-       console.log(res)
-      append(res[0].vouchers)
-}).catch(function(err){
-    console.log(err)
-})
-
- let data = JSON.parse(localStorage.getItem("purchase")) || []
-
-function append(vouchers){
-    console.log(vouchers)
-vouchers.map(function(ele){
-
-    let container = document.createElement("div")
-    container.setAttribute("class","voucher_list")
-    let card = document.createElement("div")
-    card.setAttribute("class","voucher")
-    let img = document.createElement("img")
-    img.setAttribute("class","img")
-    img.src = ele.image
-    let name = document.createElement("p")
-    name.innerText = ele.name
-    // console.log(name)
-    let price = document.createElement("p")
-    price.innerText = ele.price
-    let btn = document.createElement("button")
-    btn.innerText = "Buy"
-    btn.setAttribute("class","buy_voucher")
-    // btn.addEventListener("click" function(){
-    //     addPurchase(ele)
-    // })
-    
-    card.append(img,name,price,btn)
-    container.append(card)
-})
-
+    }
+    catch(err){
+        console.log(err)
+    }
 }
+    getData()
+
+    function append(data) {
+        let container = document.createElement("div")
+        container.setAttribute("class","voucher_list")
+        
+        data.map(function(ele){
+       
+            let card = document.createElement("div")
+            card.setAttribute("class","voucher")
+            let img = document.createElement("img")
+            img.setAttribute("class","img")
+            img.src = ele.image
+            let name = document.createElement("p")
+            name.innerText = ele.name
+            // console.log(name)
+            let price = document.createElement("p")
+            price.innerText = ele.price
+            let btn = document.createElement("button")
+            btn.innerText = "Buy"
+            btn.setAttribute("class","buy_voucher")
+            // btn.addEventListener("click" function(){
+            //     addPurchase(ele)
+            // })
+            
+            card.append(img,name,price,btn)
+            container.append(card)
+        })
+        
+        }
+    
+
+
+// fetch(url).then (function (res){
+//     return res.json()
+// }).then(function(res){
+//        console.log(res)
+//       append(res[0].vouchers)
+// }).catch(function(err){
+//     console.log(err)
+// })
+
+
 
 // function addPurchase(ele){
 
